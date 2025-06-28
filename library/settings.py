c478aea53b1f79fdb5e3b8f1db59d4a8681d82f2
Sarangsursaut <sarangsursaut10@gmail.com>
2025-06-28 16:23:54 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- library/settings.py
+++ library/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-ALLOWED_HOSTS = ['*']
+
 
 ROOT_URLCONF = 'library.urls'
 
